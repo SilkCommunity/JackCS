dcaf5b0d308cc30130eea58479b4af53f1498005
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-04-07 11:54:50 -0700

    Update to Silk.NET 2.17

--- JackCS/JackCS/JackLibraryNameContainer.cs
+++ JackCS/JackCS/JackLibraryNameContainer.cs
@@ -10,21 +10,21 @@ namespace JackCS
     internal class JackLibraryNameContainer : SearchPathContainer
     {
         /// <inheritdoc />
-        public override string Linux => "libjack.so";
+        public override string[] Linux => new[] { "libjack.so" };
 
         /// <inheritdoc />
-        public override string MacOS => "libjack.dylib";
+        public override string[] MacOS => new[] { "libjack.dylib" };
 
         /// <inheritdoc />
-        public override string Android => "libjack.so";
+        public override string[] Android => new[] { "libjack.so" };
 
         /// <inheritdoc />
-        public override string IOS => "libjack.dylib";
+        public override string[] IOS => new[] { "libjack.dylib" };
 
         /// <inheritdoc />
-        public override string Windows64 => "libjack.dll";
+        public override string[] Windows64 => new[] { "libjack.dll" };
 
         /// <inheritdoc />
-        public override string Windows86 => "libjack.dll";
+        public override string[] Windows86 => new[] { "libjack.dll" };
     }
 }
